499a3867e77bed6068c8e7bb439ea6518428e618
nabavi2 <rohullah.ghaznavi78@gmail.com>
2022-04-15 00:16:49 +0430

    added building overview screen

--- src/screens/buildingoverview/index.js
+++ src/screens/buildingoverview/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 import { Row, Column, Box } from "native-base";
-import { Entypo } from "expo-vector-icons";
+import { SearchBar } from "react-native-elements";
+import { Entypo, EvilIcons } from "expo-vector-icons";
 
 import Colors from "../../constants/Colors";
+import CustomButton from "../../components/CustomButton";
+import Layout from "../../constants/Layout";
+
+const size = Layout.window;
 
 function BuildingOverViewScreen(props) {
   return (
@@ -30,10 +35,51 @@ function BuildingOverViewScreen(props) {
           <Entypo
             name="dots-three-vertical"
             size={24}
-            color={Colors.light.black}
+            color={Colors.light.gray900}
             style={{ marginTop: 20 }}
           />
         </Box>
+      </Row>
+      <Row style={styles.inputView}>
+        <EvilIcons
+          name="search"
+          size={40}
+          color={Colors.light.gray500}
+          style={{ margin: 10 }}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          placeholderTextColor={Colors.light.gray500}
+          secureTextEntry={true}
+          keyboardType="default"
+        />
+      </Row>
+      <Row
+        style={{
+          paddingLeft: 19,
+          marginTop: 25,
+          alignItems: "center",
+          justifyContent: "space-between",
+          // alignSelf: "center",
+          paddingRight: 19,
+        }}
+      >
+        <CustomButton
+          title="29 Displayed"
+          color={Colors.light.red}
+          Size={size.width * 0.28}
+        />
+        <CustomButton
+          title="18 Soon"
+          color={Colors.light.orange}
+          Size={size.width * 0.28}
+        />
+        <CustomButton
+          title="77 Ontime"
+          color={Colors.light.primary}
+          Size={size.width * 0.28}
+        />
       </Row>
     </SafeAreaView>
   );
@@ -46,6 +92,20 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    width: "80%",
+    backgroundColor: Colors.light.white,
+    height: 45,
+    color: "#FFF",
+  },
+  inputView: {
+    width: "90%",
+    flexDirection: "row",
+    backgroundColor: Colors.light.white,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignSelf: "center",
   },
 });
 export default BuildingOverViewScreen;
